e20014a6b058d22d3b3638dc4a7585d8fdb271b8
SundarDong <brosisstudio123@gmail.com>
2025-02-03 17:03:02 +0545

    feat: add some dependencies added and understanding the basic logic of get method

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/") #This is the decorator, When you use in the ip of the / then it direct print the message
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Welcome to my API !!!"}
+
+    #if the decorator doesnt have same get url then it will run the first line of code of function.
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is my first data"}
